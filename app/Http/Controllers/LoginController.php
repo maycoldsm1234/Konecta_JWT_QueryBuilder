fa848659f4f05afb48bb8068697d321a8514f82f
maycoldsm1234 <maycoldsm1234@gmail.com>
2020-12-09 22:29:29 -0500

    Comit Inicial

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -33,7 +33,7 @@ class LoginController extends Controller
             return response()->json([
 				'type' => 'error',
 				'msg'  => 'Sin Autorizacion'
-			], 401);
+			]);
         }
 		
 		setcookie("jwt_token", $token);
